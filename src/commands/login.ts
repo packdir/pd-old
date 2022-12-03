65d226d58bbbcc0e79048386689f0daa3ec24724
henry <h@h.com>
2022-12-03 16:04:09 +0800

    new version. oclif 3

---
+++ src/commands/login.ts
@@ -0,0 +1,41 @@
+import {Command, Flags} from '@oclif/core'
+import * as inquirer from 'inquirer'
+
+export default class Login extends Command {
+  static description = 'Login to packdir.com'
+
+  //static flags = {}
+
+  //static args = [{name: 'file'}]
+
+  public async run(): Promise<void> {
+    //const {args, flags} = await this.parse(Login)
+
+    this.log('Login to packdir.com')
+    const questions = [
+      {
+        type: 'input',
+        name: 'email',
+        message: 'Email:',
+        default: ''
+      },
+      {
+        type: 'password',
+        name: 'password',
+        message: 'Password:',
+        default: ''
+      }
+    ]
+
+    inquirer
+      .prompt(questions)
+      .then( async (answers) => {
+        console.log('answers: ', answers, '')
+        console.log('Created packdir.json successfully!')
+      })
+      .catch((err) => {
+        console.log('Error: ', err)
+      })
+
+  }
+}
